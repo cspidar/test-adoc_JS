194c0665c0a74f58bebb275a062ec029e6cb727e
cspidar <32433348+cspidar@users.noreply.github.com>
2021-03-13 17:36:28 +0900

    3

--- ascii_test.js
+++ ascii_test.js
@@ -1,4 +1,5 @@
 var asciidoctor = Asciidoctor()
 
-var html = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
+var doc = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
 
+document.getElementById("target").innerHTML=doc;
