98dcf7bfd0c4d6efe69c9c5c8455ffe1929805cd
cspidar <32433348+cspidar@users.noreply.github.com>
2021-03-13 18:26:06 +0900

    자바스크립트 동작 확인-6

--- ascii_test.js
+++ ascii_test.js
@@ -1,7 +1,7 @@
 
 var asciidoctor = Asciidoctor()
 
-var doc = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
+//var doc = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
 
 document.getElementById("target_ascii").innerHTML='바뀐 메세지 입니다.';
 
