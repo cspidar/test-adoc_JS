0ec4b0fd3f9f397a9250ff578f5407b935fb3c97
cspidar <32433348+cspidar@users.noreply.github.com>
2021-03-19 18:14:01 +0900

    back to convert

--- ascii_test.js
+++ ascii_test.js
@@ -1,16 +1,16 @@
 
 // var asciidoctor = Asciidoctor()
 
-var url = './Asciidoctor_test/03_body.adoc'
+// var url = './Asciidoctor_test/03_body.adoc'
 
-var loaddoc = asciidoctor.loadFile(url)
+// var loaddoc = asciidoctor.loadFile(url)
 
-var html = asciidoctor.convert(loaddoc)
+// var html = asciidoctor.convert(loaddoc)
 
 
 // var html = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
 
-// var html = asciidoctor.convert('Hello, _Asciidoctor_')
+var html = asciidoctor.convert('Hello, _Asciidoctor_')
 console.log(html)
 
 document.getElementById("target_ascii").innerHTML=html;
