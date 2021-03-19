7f908cfe2995b2790330fdc1a5cd75e9c93d484f
Cspidar <cspidar@gmail.com>
2021-03-19 17:01:55 +0900

    plain html test - vercel -2

--- ascii_test.js
+++ ascii_test.js
@@ -5,9 +5,9 @@
 
 
 
-// var doc = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
+var html = asciidoctor.convertFile('./Asciidoctor_test/03_body.adoc', { to_file: false, standalone: true })
 
-var html = asciidoctor.convert('Hello, _Asciidoctor_')
+// var html = asciidoctor.convert('Hello, _Asciidoctor_')
 console.log(html)
 
-document.getElementById("target_ascii").innerHTML='바뀐 메세지 입니다.';
+document.getElementById("target_ascii").innerHTML=html;
